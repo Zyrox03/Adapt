5a2abeada1ac4864ad534a1e5b407e5b763d6ea3
Zyrox03 <anisanissou2003@gmail.com>
2024-02-10 22:47:03 +0100

    i think i finally fixed the cors thing

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://adapt-4cb.pages.dev/"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
 });
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://adapt-4cb.pages.dev/",
 };
 
 app.options("*", cors(corsOptions)); // include before other routes
