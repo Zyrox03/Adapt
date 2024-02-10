e87509a51b03da4294a9216e0026117dada8a0be
Zyrox03 <anisanissou2003@gmail.com>
2024-02-11 00:55:25 +0100

    i think i finally fixed the cors thing

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const sendEmailNotification = require("./sendEmailNotification");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 require("dotenv").config();
 
 
