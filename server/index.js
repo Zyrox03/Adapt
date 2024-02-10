5e3c30584c50b9f330eaf913544bd4bb7919aa9b
Zyrox03 <anisanissou2003@gmail.com>
2024-02-10 22:35:59 +0100

    i think i finally fixed the cors thing

--- server/index.js
+++ server/index.js
@@ -21,6 +21,16 @@ app.use(
   helmet({ hsts: { maxAge: 31536000, includeSubDomains: true, preload: true } })
 );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://adapt-4cb.pages.dev"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const corsOptions = {
+  origin: ["https://adapt-4cb.pages.dev", "https://www.adaptenterprise.tech", "http://localhost:5173/"],
+};
+
 app.options('*', cors(corsOptions)) // include before other routes
 
 // Routes
